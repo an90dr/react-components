c5fa8707ae7b390e86eec2c3c5976f9cfddda4dd
an90dr <an90dr@gmail.com>
2022-05-03 17:01:47 +0300

    Space size accepts small medium large and number

--- src/components/Space/Space.tsx
+++ src/components/Space/Space.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { CSSProperties, DetailedHTMLProps, HTMLAttributes } from 'react';
 import './Space.css';
+import { size } from '../../interface/SpaceType';
+import { classPrefix } from '../../utils/Constants';
 
 export type SpaceProps = {
-  
+  size: size.large | size.medium | size.small | number;
 };
-
 
 type SpaceState = {};
 
 class Space extends React.Component<SpaceProps, SpaceState> {
+  defaultButtonClass = 'rcf-space';
 
-    render(): React.ReactNode {
-        return <span className='rfc-space'></span>
+  render(): React.ReactNode {
+    return (
+      <span
+        className={this.defaultButtonClass + ' ' + this._resolveSizeClassName()}
+        style={this._resolveSizeStyleProperty()}
+      ></span>
+    );
+  }
+
+  _resolveSizeClassName(): string {
+    switch (this.props.size) {
+      case size.small:
+        return classPrefix + '-' + size.small;
+      case size.medium:
+        return classPrefix + '-' + size.medium;
+      case size.large:
+        return classPrefix + '-' + size.large;
+      default:
+        return '';
     }
+  }
 
+  _resolveSizeStyleProperty(): CSSProperties | undefined {
+    if (typeof this.props.size === 'number') {
+      return { marginLeft: this.props.size / 2, marginRight: this.props.size / 2 };
+    }
+  }
 }
 
 export default Space;
